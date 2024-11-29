3da5b7a760a008a50ba86d73bc4a3615e6938944
Sagar <patelsagar913@gmail.com>
2024-11-29 21:10:56 +0530

    spring-boot just initialized everything

---
+++ src/test/java/com/shopkart/productservicecapstone/ProductServiceCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopkart.productservicecapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
